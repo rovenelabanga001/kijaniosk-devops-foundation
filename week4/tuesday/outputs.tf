0396cbcd110c0a223d5cf4ea812d0368c7833f5d
rovebelabanga <rovenelabanga2001@gmail.com>
2026-04-04 21:44:49 +0300

    terraform modules

--- week4/tuesday/outputs.tf
+++ week4/tuesday/outputs.tf
@@ -1,21 +1,20 @@
-output "api_server_public_ip" {
-    description = "Public API adress for the Kijanikiosk API server"
-    value = google_compute_instance.kk_api.network_interface[0].access_config[0].nat_ip
+output "server_ips" {
+    description = "Public API addresses for all servers"
+    value = {for k, v in module.app_servers : k => v.public_ip}
 }
 
-output "ssh_command" {
-    description = "SSH command to connect to the API server"
-    value = "ssh -i ~/.ssh/kijanikiosk-key ${var.ssh_user}@${google_compute_instance.kk_api.network_interface[0].access_config[0].nat_ip}"
+output "ssh_commands" {
+    description = "SSH commands for all servers"
+    value = {for k, v in module.app_servers : k => v.ssh_command}
+}
+
+# output "instance_name" {
+#     description = "Name of the compute instance"
+#     value = google_compute_instance.kk_api.name
+# }
+
+# output "instance_zone" {
+#     description = "Zone where the instance is runninng"
+#     value = google_compute_instance.kk_api.zone
   
-}
-
-output "instance_name" {
-    description = "Name of the compute instance"
-    value = google_compute_instance.kk_api.name
-}
-
-output "instance_zone" {
-    description = "Zone where the instance is runninng"
-    value = google_compute_instance.kk_api.zone
-  
-}
+# }
